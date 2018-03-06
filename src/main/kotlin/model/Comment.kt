5185cccde4ec79a8fcba3ea325d24dc0770db07b
Ryan Harrison <mail@ryanharrison.co.uk>
2018-03-06 21:26:05 +0000

    Add Exposed and model for comments

---
+++ src/main/kotlin/model/Comment.kt
@@ -0,0 +1,25 @@
+package model
+
+import org.jetbrains.exposed.sql.Table
+
+object Comments : Table() {
+    val id = varchar("id", 12).primaryKey()
+    val plainText = text("plainText")
+    val markdownText = text("markdownText")
+    val dateCreated = long("dateCreated")
+}
+
+
+data class Comment(
+        val id: String,
+        val entryId: String,
+        val plainText: String,
+        val markdownText: String,
+        val dateCreated: Long
+)
+
+
+data class NewComment(
+        val id: String?,
+        val plainText: String
+)
