2890e7697edfd4170823e87a935aaaf1cb6bfc25
Ryan Harrison <mail@ryanharrison.co.uk>
2018-03-06 21:19:09 +0000

    Setup ktor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,33 @@
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun Application.module() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(ContentNegotiation) {
+        jackson {
+            configure(SerializationFeature.INDENT_OUTPUT, true)
+        }
+    }
+
+    install(Routing) {
+        get("/") {
+            call.respond("Hello World")
+        }
+    }
+}
+
 fun main(args: Array<String>) {
-    println("Main")
+    embeddedServer(Netty, 8080, watchPaths = listOf("Main"), module = Application::module).start()
 }
