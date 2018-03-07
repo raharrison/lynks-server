ce27a56e4821cc347d3a7ef4a86229b4fa4bff89
Ryan Harrison <mail@ryanharrison.co.uk>
2018-03-07 21:00:04 +0000

    Models for links and note subentries

---
+++ src/main/kotlin/model/Link.kt
@@ -0,0 +1,24 @@
+package model
+
+data class Link(
+        val id: String,
+        val title: String,
+        val url: String,
+        val source: String,
+        val dateUpdated: Long,
+        val tags: List<Tag>
+) : Entry {
+    @JvmField
+    val type = EntryType.LINK
+}
+
+
+data class NewLink(
+        var id: String? = null,
+        val title: String,
+        val url: String,
+        val tags: List<String>
+) : NewEntry {
+    override fun id(): String? = id
+    override fun tags(): List<String> = tags
+}
---
+++ src/main/kotlin/model/Note.kt
@@ -0,0 +1,24 @@
+package model
+
+data class Note(
+        val id: String,
+        val title: String,
+        val plainText: String,
+        val markdownText: String,
+        val dateUpdated: Long,
+        val tags: List<Tag>
+) : Entry {
+    @JvmField
+    val type = EntryType.NOTE
+}
+
+
+data class NewNote(
+        var id: String? = null,
+        val title: String,
+        val plainText: String,
+        val tags: List<String>
+) : NewEntry {
+    override fun id(): String? = id
+    override fun tags(): List<String> = tags
+}
