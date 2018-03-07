ce8c1fb515f343dad154f740d49bdb3b167aba08
Ryan Harrison <mail@ryanharrison.co.uk>
2018-03-07 20:59:07 +0000

    Model for tags

---
+++ src/main/kotlin/model/Tag.kt
@@ -0,0 +1,31 @@
+package model
+
+import org.jetbrains.exposed.sql.Table
+
+object Tags : Table() {
+    val id = varchar("id", 12).primaryKey()
+    val name = varchar("name", 255)
+    val parentId = (varchar("parentId", 12) references Tags.id).nullable()
+    val dateUpdated = long("dateCreated")
+}
+
+object EntryTags : Table() {
+    val tagId = (varchar("tagId", 12) references Tags.id).primaryKey()
+    val entryId = (varchar("entryId", 12) references Entries.id).primaryKey()
+}
+
+data class Tag(
+        val id: String,
+        val name: String,
+        val children: MutableList<Tag>,
+        val dateUpdated: Long
+) {
+    override fun hashCode(): Int = id.hashCode()
+    override fun equals(other: Any?): Boolean = if (other is Tag) id == other.id else false
+}
+
+data class NewTag(
+        val id: String?,
+        val name: String,
+        val parentId: String?
+)
