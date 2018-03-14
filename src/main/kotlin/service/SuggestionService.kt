04495ee1deead23674d60f59f273229b2287f523
Ryan Harrison <mail@ryanharrison.co.uk>
2018-03-14 20:43:55 +0000

    Save images as temp files after suggestion

--- src/main/kotlin/service/SuggestionService.kt
+++ src/main/kotlin/service/SuggestionService.kt
@@ -1,35 +1,23 @@
 package service
 
-import com.chimbori.crux.articles.ArticleExtractor
-import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.withContext
 import link.WebpageExtractor
 import model.FileType
 import model.Suggestion
-import java.net.URL
 
-class SuggestionService(val fileService: FileService) {
+class SuggestionService(private val fileService: FileService) {
 
-    suspend fun suggest(suggestion: Suggestion): Suggestion {
-        val html = async {
-            URL(suggestion.url).readText()
+    suspend fun processLinkAsync(url: String): Suggestion = withContext(CommonPool) {
+        WebpageExtractor(url).use {
+            val thumb = it.generateThumbnail()
+            val screen = it.generateScreenshot()
+            val title = it.title
+            val cleanUrl = it.resolvedUrl
+            val thumbPath = fileService.saveTempFile(url, thumb, FileType.THUMBNAIL)
+            val screenPath = fileService.saveTempFile(url, screen, FileType.SCREENSHOT)
+            Suggestion(cleanUrl, title, thumbPath, screenPath)
         }
-
-        val article = ArticleExtractor.with(suggestion.url, html.await())
-                .extractMetadata()
-                .extractContent()
-                .article()
-        return Suggestion(suggestion.url, article.title, article.imageUrl)
     }
-
-    suspend fun processLink(url: String): Suggestion =
-            WebpageExtractor(url).use {
-                val thumb = it.generateThumbnail()
-                val screen = it.generateScreenshot()
-                val title = it.title
-                val cleanUrl = it.resolvedUrl
-                val thumbPath = fileService.saveTempFile(url, thumb, FileType.THUMBNAIL)
-                val screenPath = fileService.saveTempFile(url, screen, FileType.SCREENSHOT)
-                Suggestion(cleanUrl, title, thumbPath, screenPath)
-            }
 
 }
--- src/main/kotlin/web/SuggestionResource.kt
+++ src/main/kotlin/web/SuggestionResource.kt
@@ -1,18 +1,17 @@
 package web
 
 import io.ktor.application.call
-import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
-import io.ktor.routing.post
-import model.Suggestion
+import io.ktor.routing.get
 import service.SuggestionService
 
 fun Route.suggest(suggestionService: SuggestionService) {
 
-    post("/suggest") {
-        val suggestion = call.receive<Suggestion>()
-        call.respond(suggestionService.suggest(suggestion))
+    get("/suggest") {
+        val url = call.parameters["url"]
+        if (url == null) call.respond("url parameter required")
+        else call.respond(suggestionService.processLinkAsync(url))
     }
 
 }
