ecaa2e7f0b6f5a6769e11e3718333a58eba8bdae
Ryan Harrison <mail@ryanharrison.co.uk>
2018-03-25 19:43:10 +0100

    Centralised json object mapper

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,23 @@
-import com.fasterxml.jackson.databind.SerializationFeature
 import db.DatabaseFactory
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
-import io.ktor.jackson.jackson
+import io.ktor.http.ContentType
+import io.ktor.jackson.JacksonConverter
 import io.ktor.routing.Routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import service.*
+import util.JsonMapper.defaultMapper
 import web.*
 
 fun Application.module() {
     install(DefaultHeaders)
     install(CallLogging)
     install(ContentNegotiation) {
-        jackson {
-            configure(SerializationFeature.INDENT_OUTPUT, true)
-        }
+        register(ContentType.Application.Json, JacksonConverter(defaultMapper))
     }
 
     DatabaseFactory()
---
+++ src/main/kotlin/util/JsonMapper.kt
@@ -0,0 +1,16 @@
+package util
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+
+object JsonMapper {
+
+    val defaultMapper: ObjectMapper = jacksonObjectMapper()
+
+    init {
+        defaultMapper.configure(SerializationFeature.INDENT_OUTPUT, true)
+        // defaultMapper.registerModule(JavaTimeModule())
+    }
+
+}
