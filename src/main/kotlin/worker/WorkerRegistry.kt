7d0001ebf4474b6286e34b8b7c4aa9a1f2ed9e5c
Ryan Harrison <mail@ryanharrison.co.uk>
2018-04-01 20:16:43 +0100

    Add registry to coordinate all workers

---
+++ src/main/kotlin/worker/WorkerRegistry.kt
@@ -0,0 +1,15 @@
+package worker
+
+import link.LinkProcessingRequest
+import link.LinkProcessorWorker
+import resource.ResourceManager
+
+class WorkerRegistry(resourceManager: ResourceManager) {
+
+    private val linkWorker = LinkProcessorWorker(resourceManager).worker()
+
+    fun acceptLinkWork(request: LinkProcessingRequest) {
+        linkWorker.offer(request)
+    }
+
+}
