bc6c03a07e334d99bc0668a9186ddafd08ec963d
Ryan Harrison <mail@ryanharrison.co.uk>
2018-04-08 17:31:12 +0100

    Post suggestions to support url params

--- src/main/kotlin/suggest/SuggestionResource.kt
+++ src/main/kotlin/suggest/SuggestionResource.kt
@@ -1,16 +1,16 @@
 package suggest
 
 import io.ktor.application.call
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
-import io.ktor.routing.get
+import io.ktor.routing.post
 
 fun Route.suggest(suggestionService: SuggestionService) {
 
-    get("/suggest") {
-        val url = call.parameters["url"]
-        if (url == null) call.respond("url parameter required")
-        else call.respond(suggestionService.processLink(url))
+    post("/suggest") {
+        val url = call.receive<String>()
+        call.respond(suggestionService.processLink(url))
     }
 
 }
