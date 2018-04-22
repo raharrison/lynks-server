bcbcf2f7def527c3403c627108e9f18d0ffac399
Ryan Harrison <mail@ryanharrison.co.uk>
2018-04-22 16:46:46 +0100

    Tests for suggestion service

---
+++ src/test/kotlin/service/SuggestionServiceTest.kt
@@ -0,0 +1,33 @@
+package service
+
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.experimental.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import suggest.Suggestion
+import suggest.SuggestionService
+import worker.SuggestLinkProcessingRequest
+import worker.WorkerRegistry
+
+class SuggestionServiceTest {
+
+    @Test
+    fun testSuggestion() {
+        val workerRegistry = mockk<WorkerRegistry>()
+        val service = SuggestionService(workerRegistry)
+
+        val suggestion = Suggestion("google.com", "Google", null, null)
+
+        every { workerRegistry.acceptLinkWork(any()) } answers {
+            val req = this.firstArg<SuggestLinkProcessingRequest>()
+            req.response.complete(suggestion)
+        }
+
+        runBlocking {
+            val response = service.processLink("google.com")
+            assertThat(response).isEqualTo(suggestion)
+        }
+    }
+
+}
