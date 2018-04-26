8dd9d29b4f38c7509e59412cb05e1183e7ef3b2c
Ryan Harrison <mail@ryanharrison.co.uk>
2018-04-26 17:29:48 +0100

    Convert tasks in props to list

--- src/main/kotlin/common/Properties.kt
+++ src/main/kotlin/common/Properties.kt
@@ -3,7 +3,7 @@ package common
 open class BaseProperties {
 
     val attributes = mutableMapOf<String, String>()
-    val tasks = mutableMapOf<String, TaskDefinition>()
+    val tasks = mutableListOf<TaskDefinition>()
 
     fun addAttribute(key: String, value: String) {
         attributes[key] = value
@@ -14,8 +14,8 @@ open class BaseProperties {
     fun containsAttribute(key: String) = attributes.contains(key)
 
     fun addTask(task: TaskDefinition) {
-        tasks[task.id] = task
+        tasks.add(task)
     }
 
-    fun getTask(id: String): TaskDefinition? = tasks[id]
+    fun getTask(id: String): TaskDefinition? = tasks.find { it.id == id }
 }
