22536804d2c0953db091b9aad83a1c327fa18cd6
Ryan Harrison <mail@ryanharrison.co.uk>
2018-06-20 21:20:20 +0100

    Tests for link suggestions

--- src/test/kotlin/worker/LinkProcessorWorkerTest.kt
+++ src/test/kotlin/worker/LinkProcessorWorkerTest.kt
@@ -5,13 +5,16 @@ import common.Link
 import common.TestCoroutineContext
 import entry.LinkService
 import io.mockk.*
+import kotlinx.coroutines.experimental.CompletableDeferred
 import kotlinx.coroutines.experimental.runBlocking
 import link.ImageResource
 import link.LinkProcessor
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import resource.HTML
 import resource.ResourceManager
 import resource.ResourceType
+import suggest.Suggestion
 
 class LinkProcessorWorkerTest {
 
@@ -56,4 +59,81 @@ class LinkProcessorWorkerTest {
         verify(exactly = 1) { resourceManager.saveGeneratedResource(link.id, ResourceType.DOCUMENT, HTML, html.toByteArray()) }
     }
 
+    @Test
+    fun testDefaultSuggest() = runBlocking(TestCoroutineContext()) {
+        val url = "google.com"
+
+        val thumb = ImageResource(byteArrayOf(1,2,3), ".jpg")
+        val screen = ImageResource(byteArrayOf(4,5,6), ".png")
+        val html = "<html>"
+        val processor = mockk<LinkProcessor>()
+
+        val thumbPath = "thumbPath"
+        val screenPath = "screenPath"
+        val resolvedUrl = "resolvedUrl"
+        val title = "title"
+
+        coEvery { processor.generateThumbnail() } returns thumb
+        coEvery { processor.generateScreenshot() } returns screen
+        coEvery { processor.html } returns html
+        every { processor.title } returns title
+        every { processor.resolvedUrl } returns resolvedUrl
+        every { processor.close() } just Runs
+
+        coEvery { processorFactory.createProcessors(url) } returns listOf(processor)
+        every { resourceManager.saveTempFile(url, thumb.image, ResourceType.THUMBNAIL, thumb.extension) } returns thumbPath
+        every { resourceManager.saveTempFile(url, screen.image, ResourceType.SCREENSHOT, screen.extension) } returns screenPath
+        every { resourceManager.saveTempFile(url, html.toByteArray(), ResourceType.DOCUMENT, HTML) } returns screenPath
+
+        val deferred = CompletableDeferred<Suggestion>()
+        val channel = worker.apply { runner = coroutineContext }.worker()
+        channel.send(SuggestLinkProcessingRequest(url, deferred))
+        channel.close()
+
+        val suggestion = deferred.await()
+        assertThat(suggestion.title).isEqualTo(title)
+        assertThat(suggestion.url).isEqualTo(resolvedUrl)
+        assertThat(suggestion.thumbnail).isEqualTo(thumbPath)
+        assertThat(suggestion.screenshot).isEqualTo(screenPath)
+
+        coVerify(exactly = 1) { processorFactory.createProcessors(url) }
+        verify(exactly = 1) { processor.close() }
+
+        coVerify(exactly = 1) { processor.generateThumbnail() }
+        coVerify(exactly = 1) { processor.generateScreenshot() }
+        coVerify(exactly = 1) { processor.html }
+
+        verify(exactly = 1) { resourceManager.saveTempFile(url, thumb.image, ResourceType.THUMBNAIL, thumb.extension) }
+        verify(exactly = 1) { resourceManager.saveTempFile(url,screen.image, ResourceType.SCREENSHOT, screen.extension) }
+        verify(exactly = 1) { resourceManager.saveTempFile(url, html.toByteArray(), ResourceType.DOCUMENT, HTML) }
+    }
+
+    @Test
+    fun testSuggestCompletedExceptionally() = runBlocking(TestCoroutineContext()) {
+        val url = "google.com"
+
+        val processor = mockk<LinkProcessor>()
+
+        val exception = RuntimeException("error during computation")
+        coEvery { processor.generateThumbnail() } throws exception
+        every { processor.close() } just Runs
+
+        coEvery { processorFactory.createProcessors(url) } returns listOf(processor)
+
+        val deferred = CompletableDeferred<Suggestion>()
+        val channel = worker.apply { runner = coroutineContext }.worker()
+        channel.send(SuggestLinkProcessingRequest(url, deferred))
+        channel.close()
+
+        assertThat(deferred.isCompletedExceptionally).isTrue()
+        assertThat(deferred.getCompletionExceptionOrNull()).isEqualTo(exception)
+
+        coVerify(exactly = 1) { processorFactory.createProcessors(url) }
+        verify(exactly = 1) { processor.close() }
+
+        coVerify(exactly = 1) { processor.generateThumbnail() }
+        coVerify(exactly = 1) { processor.generateScreenshot() }
+        coVerify(exactly = 0) { processor.html }
+        verify(exactly = 0) { resourceManager.saveTempFile(any(), any(), any(), any()) }
+    }
 }
