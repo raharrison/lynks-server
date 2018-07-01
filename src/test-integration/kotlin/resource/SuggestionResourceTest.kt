d665647a45f660ac0fe03696ec48c243d4772395
Ryan Harrison <mail@ryanharrison.co.uk>
2018-07-01 12:23:08 +0100

    Tests for suggestion endpoint

---
+++ src/test-integration/kotlin/resource/SuggestionResourceTest.kt
@@ -0,0 +1,47 @@
+package resource
+
+import common.ServerTest
+import io.restassured.RestAssured.given
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import suggest.Suggestion
+
+class SuggestionResourceTest: ServerTest() {
+
+    @Test
+    fun testInvalidUrlReturnsBadRequest() {
+        given()
+                .body("invalid")
+                .post("/suggest")
+                .then()
+                    .statusCode(400)
+    }
+
+    @Test
+    fun testGetSuggestion() {
+        val suggestion = given()
+                .body("https://ryanharrison.co.uk")
+                .post("/suggest")
+                .then()
+                .extract().`as`(Suggestion::class.java)
+        assertThat(suggestion.url).isEqualTo("https://ryanharrison.co.uk/")
+        assertThat(suggestion.title).isEqualTo("Ryan Harrison - My blog, portfolio and technology related ramblings")
+        assertThat(suggestion.screenshot).isNotNull()
+        assertThat(suggestion.thumbnail).isNotNull()
+        //TODO: test retrieval of static temp files
+    }
+
+    @Test
+    fun testYoutubeSuggestion() {
+        val suggestion = given()
+                .body("https://www.youtube.com/watch?v=DAiEUeM8Uv0")
+                .post("/suggest")
+                .then()
+                .extract().`as`(Suggestion::class.java)
+        assertThat(suggestion.url).isEqualTo("https://www.youtube.com/watch?v=DAiEUeM8Uv0")
+        assertThat(suggestion.title).isEqualTo("Savoy - How U Like Me Now (feat. Roniit) [Monstercat Release]")
+        assertThat(suggestion.screenshot).isNull()
+        assertThat(suggestion.thumbnail).isNotNull()
+    }
+
+}
