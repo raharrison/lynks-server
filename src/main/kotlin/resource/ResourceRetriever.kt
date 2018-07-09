a0be1309b776853b1960fc594e5d0d00a4a467c8
Ryan Harrison <mail@ryanharrison.co.uk>
2018-07-09 19:58:44 +0100

    Update Fuel and use new coroutine API

--- src/main/kotlin/resource/ResourceRetriever.kt
+++ src/main/kotlin/resource/ResourceRetriever.kt
@@ -1,10 +1,9 @@
 package resource
 
-import com.github.kittinunf.fuel.core.*
+import awaitByteArrayResult
+import awaitStringResult
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
-import kotlinx.coroutines.experimental.suspendCancellableCoroutine
-import java.nio.charset.Charset
 
 interface ResourceRetriever {
 
@@ -14,37 +13,10 @@ interface ResourceRetriever {
 
 }
 
-private suspend fun <T : Any, U : Deserializable<T>> Request.await(
-        deserializable: U
-): Triple<Request, Response, Result<T, FuelError>> =
-        suspendCancellableCoroutine { continuation ->
-            continuation.invokeOnCancellation {
-                if (continuation.isCancelled) {
-                    continuation.cancel()
-                }
-            }
-
-            response(deserializable) { request: Request, response: Response, result: Result<T, FuelError> ->
-                result.fold({
-                    continuation.resume(Triple(request, response, result))
-                }, {
-                    continuation.resumeWithException(it.exception)
-                })
-            }
-        }
-
-suspend fun Request.awaitResponse(): Triple<Request, Response, Result<ByteArray, FuelError>> =
-        await(Request.byteArrayDeserializer())
-
-suspend fun Request.awaitString(
-        charset: Charset = Charsets.UTF_8
-): Triple<Request, Response, Result<String, FuelError>> = await(Request.stringDeserializer(charset))
-
-
 class WebResourceRetriever : ResourceRetriever {
 
     override suspend fun getString(location: String): String? = try {
-        val result = location.httpGet().awaitString().third
+        val result = location.httpGet().awaitStringResult()
         when (result) {
             is Result.Success -> result.get()
             else -> null
@@ -54,7 +26,7 @@ class WebResourceRetriever : ResourceRetriever {
     }
 
     override suspend fun getFile(location: String): ByteArray? = try {
-        val result = location.httpGet().awaitResponse().third
+        val result = location.httpGet().awaitByteArrayResult()
         when (result) {
             is Result.Success -> result.get()
             else -> null
