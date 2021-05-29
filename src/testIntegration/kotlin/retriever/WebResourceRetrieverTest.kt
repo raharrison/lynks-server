37d69e1c6cbef5cd7ed9cad9020adac7ddc00881
Ryan Harrison <mail@ryanharrison.co.uk>
2021-05-29 20:56:53 +0100

    Integration tests for web resource retrieval and suggestion api

--- src/testIntegration/kotlin/retriever/WebResourceRetrieverTest.kt
+++ src/testIntegration/kotlin/retriever/WebResourceRetrieverTest.kt
@@ -1,21 +1,40 @@
 package retriever
 
-import common.ServerTest
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration.options
 import common.exception.ExecutionException
-import io.restassured.RestAssured
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.fail
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import resource.WebResourceRetriever
-import util.JsonMapper
 import util.Result
 
-class WebResourceRetrieverTest: ServerTest() {
+class WebResourceRetrieverTest {
 
     private val retriever = WebResourceRetriever()
-    private val expected = JsonMapper.defaultMapper.writeValueAsString(mapOf("status" to "ok"))
-    private val baseUrl = "${RestAssured.baseURI}:${RestAssured.port}${RestAssured.basePath}"
+    private val baseUrl = "http://localhost:3893/api"
+
+    private lateinit var wireMockServer: WireMockServer
+
+    @BeforeEach
+    fun before() {
+        wireMockServer = WireMockServer(
+            options()
+                .port(3893)
+        )
+        wireMockServer.start()
+    }
+
+    @AfterEach
+    fun after() {
+        wireMockServer.stop()
+        wireMockServer.resetAll()
+    }
 
     @Test
     fun testInvalidUrlReturnsNull() = runBlocking {
@@ -24,67 +43,145 @@ class WebResourceRetrieverTest: ServerTest() {
     }
 
     @Test
-    fun testInvalidPathReturnsNull()= runBlocking {
+    fun testInvalidPathReturnsNull() = runBlocking {
         assertThat(retriever.getString("$baseUrl/invalid")).isNull()
         assertThat(retriever.getFile("$baseUrl/invalid")).isNull()
     }
 
-    @Test
-    fun testGetString()= runBlocking {
-        val res = retriever.getString("$baseUrl/health")
-        assertThat(res).isEqualTo(expected)
-        Unit
-    }
+    @Nested
+    inner class GetString {
 
-    @Test
-    fun testGetFile()= runBlocking {
-        val res = retriever.getFile("$baseUrl/health")
-        assertThat(res).isEqualTo(expected.toByteArray())
-        Unit
-    }
-
-    @Test
-    fun testGetStringResult() = runBlocking {
-        val res = retriever.getStringResult("$baseUrl/health")
-        if(res is Result.Success) {
-            assertThat(res.value).isEqualTo(expected)
-        } else {
-            fail("Result is not Success")
+        @Test
+        fun testGetStringSuccess() = runBlocking {
+            val expected = "response"
+            wireMockServer.stubFor(
+                get("/api/stringResponse")
+                    .willReturn(ok(expected))
+            )
+            val res = retriever.getString("$baseUrl/stringResponse")
+            assertThat(res).isEqualTo(expected)
+            Unit
         }
-        Unit
+
+        @Test
+        fun testGetStringResult() = runBlocking {
+            val expected = "response"
+            wireMockServer.stubFor(
+                get("/api/stringResponse")
+                    .willReturn(ok(expected))
+            )
+            val res = retriever.getStringResult("$baseUrl/stringResponse")
+            if (res is Result.Success) {
+                assertThat(res.value).isEqualTo(expected)
+            } else {
+                fail("Result is not Success")
+            }
+            Unit
+        }
+
+        @Test
+        fun testGetStringResultFailure() = runBlocking {
+            wireMockServer.stubFor(
+                get("/api/failedStringResponse")
+                    .willReturn(status(500))
+            )
+            val res = retriever.getStringResult("$baseUrl/failedStringResponse")
+            if (res is Result.Failure) {
+                assertThat(res.reason).isInstanceOf(ExecutionException::class.java)
+            } else {
+                fail("Result is not Failure")
+            }
+            Unit
+        }
     }
 
-    @Test
-    fun testGetStringResultFailure() = runBlocking {
-        val res = retriever.getStringResult("$baseUrl/invalid")
-        if(res is Result.Failure) {
-            assertThat(res.reason).isInstanceOf(ExecutionException::class.java)
-        } else {
-            fail("Result is not Failure")
+    @Nested
+    inner class GetFile {
+
+        @Test
+        fun testGetFile() = runBlocking {
+            val expected = "response"
+            wireMockServer.stubFor(
+                get("/api/fileResponse")
+                    .willReturn(ok(expected))
+            )
+            val res = retriever.getFile("$baseUrl/fileResponse")
+            assertThat(res).isEqualTo(expected.toByteArray())
+            Unit
         }
-        Unit
+
+        @Test
+        fun testGetFileResult() = runBlocking {
+            val expected = "response"
+            wireMockServer.stubFor(
+                get("/api/fileResponse")
+                    .willReturn(ok(expected))
+            )
+            val res = retriever.getFileResult("$baseUrl/fileResponse")
+            if (res is Result.Success) {
+                assertThat(res.value).isEqualTo(expected.toByteArray())
+            } else {
+                fail("Result is not Success")
+            }
+            Unit
+        }
+
+        @Test
+        fun testGetFileResultFailure() = runBlocking {
+            wireMockServer.stubFor(
+                get("/api/failedFileResponse")
+                    .willReturn(status(500))
+            )
+            val res = retriever.getStringResult("$baseUrl/failedFileResponse")
+            if (res is Result.Failure) {
+                assertThat(res.reason).isInstanceOf(ExecutionException::class.java)
+            } else {
+                fail("Result is not Failure")
+            }
+            Unit
+        }
     }
 
-    @Test
-    fun testGetFileResult() = runBlocking {
-        val res = retriever.getFileResult("$baseUrl/health")
-        if(res is Result.Success) {
-            assertThat(res.value).isEqualTo(expected.toByteArray())
-        } else {
-            fail("Result is not Success")
-        }
-        Unit
-    }
+    @Nested
+    inner class PostString {
 
-    @Test
-    fun testGetFileResultFailure() = runBlocking {
-        val res = retriever.getStringResult("$baseUrl/invalid")
-        if(res is Result.Failure) {
-            assertThat(res.reason).isInstanceOf(ExecutionException::class.java)
-        } else {
-            fail("Result is not Failure")
+        @Test
+        fun testPostStringResultSuccess() = runBlocking {
+            val expected = "response"
+            val body = mapOf("key" to "value")
+            wireMockServer.stubFor(
+                post("/api/postString")
+                    .withHeader("Content-Type", equalTo("application/json"))
+                    .withRequestBody(equalTo("""{"key":"value"}"""))
+                    .willReturn(ok(expected))
+            )
+            val res = retriever.postStringResult("$baseUrl/postString", body)
+            if (res is Result.Success) {
+                assertThat(res.value).isEqualTo(expected)
+            } else {
+                fail("Result is not Success")
+            }
+            Unit
         }
-        Unit
+
+        @Test
+        fun testPostStringResultFailure() = runBlocking {
+            val body = """{"key": "value"}"""
+            wireMockServer.stubFor(
+                post("/api/postString")
+                    .withHeader("Content-Type", equalTo("application/json"))
+                    .withRequestBody(equalTo(body))
+                    .willReturn(status(500))
+            )
+            val res = retriever.postStringResult("$baseUrl/failedPostString", body)
+            if (res is Result.Failure) {
+                assertThat(res.reason).isInstanceOf(ExecutionException::class.java)
+            } else {
+                fail("Result is not Failure")
+            }
+            Unit
+        }
+
     }
 
 }
