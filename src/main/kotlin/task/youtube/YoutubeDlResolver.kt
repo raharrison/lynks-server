22f7e2e452edc649f0dc0aecfe1f1607f538bff8
Ryan Harrison <mail@ryanharrison.co.uk>
2021-08-06 18:11:46 +0100

    Set execute permissions

--- src/main/kotlin/task/youtube/YoutubeDlResolver.kt
+++ src/main/kotlin/task/youtube/YoutubeDlResolver.kt
@@ -17,19 +17,20 @@ class YoutubeDlResolver(private val resourceRetriever: WebResourceRetriever) {
     private val log = loggerFor<YoutubeDlResolver>()
 
     suspend fun resolveYoutubeDl(): String {
-        val binaryName = "youtube-dl${if(SystemUtils.IS_OS_WINDOWS) ".exe" else ""}"
+        val binaryName = "youtube-dl${if (SystemUtils.IS_OS_WINDOWS) ".exe" else ""}"
         val binaryPath = Paths.get(Environment.resource.binaryBasePath, binaryName)
-        if(binaryPath.exists()) {
+        if (binaryPath.exists()) {
             log.info("Youtube-dl binary resolved to {}", binaryPath)
         } else {
             val youtubeDlHost = Environment.external.youtubeDlHost
             log.info("No youtube-dl binary found, retrieving from: {}", youtubeDlHost)
-            when(val result = resourceRetriever.getFileResult("${youtubeDlHost}/${binaryName}")) {
+            when (val result = resourceRetriever.getFileResult("${youtubeDlHost}/${binaryName}")) {
                 is Result.Failure -> throw result.reason
                 is Result.Success -> {
                     val bytes = result.value
                     withContext(Dispatchers.IO) {
                         FileUtils.writeToFile(binaryPath, bytes)
+                        binaryPath.toFile().setExecutable(true)
                     }
                     log.info("Youtube-dl binary successfully saved to {}", binaryPath)
                 }
