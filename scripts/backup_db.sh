9934e72464a518c469f17fd71c91128be2c9264c
Ryan Harrison <mail@ryanharrison.co.uk>
2022-03-28 19:22:00 +0100

    Use .env file in backup script

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-docker exec -t lynks_postgres_1 pg_dumpall -c -U lynksuser -d lynksdb > /tmp/db.dump
-docker cp lynks_postgres_1:/tmp/db.dump dump_$(date +"%Y-%m-%d_%H_%M_%S").sql
+set -e
+
+if [ $# -eq 0 ]; then
+    echo "Expected path to config directory"
+    exit 1
+fi
+
+CONFIG_PATH=$1
+
+source "$CONFIG_PATH"
+
+DUMP_FILENAME=dump_$(date +"%Y-%m-%d_%H_%M_%S").sql
+
+docker exec -t lynks_postgres_1 pg_dumpall -c -U "$POSTGRES_USER" -d "$POSTGRES_DB" > /tmp/db.dump
+docker cp lynks_postgres_1:/tmp/db.dump "$DUMP_FILENAME"
+
+echo "Database dump saved to ${DUMP_FILENAME}"
+
 
 # Restore
 # docker cp db.dump lynks_postgres_1:/tmp/db_restore.dump
