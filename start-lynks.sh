c1044acc151bcf46c4e22fa48131e1e18b954c07
Ryan Harrison <mail@ryanharrison.co.uk>
2022-03-30 14:07:27 +0100

    Create default media directory if doesn't exist

--- start-lynks.sh
+++ start-lynks.sh
@@ -3,4 +3,9 @@
 export CURRENT_UID=$(id -u)
 export CURRENT_GUID=$(id -g)
 
+if [ ! -d "media" ]; then
+    echo "Media directory not found for volume mount, creating.."
+    mkdir media
+fi
+
 docker-compose --env-file ./config/.env up "$@"
