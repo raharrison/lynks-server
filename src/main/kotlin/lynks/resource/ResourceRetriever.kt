9df1ce1b9db57100e3df1fbe453cf17adeeee4fd
Ryan Harrison <mail@ryanharrison.co.uk>
2023-12-04 01:53:33 +0000

    Set user agent header on all requests

--- src/main/kotlin/lynks/resource/ResourceRetriever.kt
+++ src/main/kotlin/lynks/resource/ResourceRetriever.kt
@@ -116,6 +116,7 @@ class WebResourceRetriever : ResourceRetriever {
 
     private fun createBaseRequest(location: String): HttpRequest.Builder {
         val builder = HttpRequest.newBuilder(URI.create(location))
+            .header("User-Agent", USER_AGENT)
         val requestId = MDC.get(MDC_REQUEST_ID)
         if (StringUtils.isNotEmpty(requestId)) {
             builder.setHeader(HttpHeaders.XRequestId, requestId)
@@ -124,6 +125,7 @@ class WebResourceRetriever : ResourceRetriever {
     }
 
     companion object {
+        private const val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0"
         private val client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build()
         private val log = loggerFor<WebResourceRetriever>()
     }
